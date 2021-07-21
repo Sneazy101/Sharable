eaad88c7a9494aface6f3d21302445161b12635e
mba118 <mohammad_bin_zafar_shahid@sfu.ca>
2021-07-21 01:32:46 -0700

    Basic Log in Log off complete

--- app/src/main/java/com/example/sharable_dead/MainActivity.java
+++ app/src/main/java/com/example/sharable_dead/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.sharable_dead;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.sharable_dead.databinding.ActivityMainBinding;
+import com.example.sharable_dead.databinding.ActivitySignInBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.settings:
+                Toast.makeText(this, "Clicked Settings", Toast.LENGTH_SHORT);
+                break;
+
+            case R.id.logout:
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                Toast.makeText(this, "Logged Out", Toast.LENGTH_SHORT);
+                break;
+
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/sharable_dead/SignUpActivity.java
+++ app/src/main/java/com/example/sharable_dead/SignUpActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -64,40 +65,13 @@ public class SignUpActivity extends AppCompatActivity {
                 });
             }
         });
-    }
 
-//    // [START on_start_check_user]
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        if(currentUser != null){
-//            reload();
-//        }
-//    }
-//    // [END on_start_check_user]
-//
-//    private void createAccount(String email, String password) {
-//        // [START create_user_with_email]
-//        mAuth.createUserWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d(TAG, "createUserWithEmail:success");
-//                            FirebaseUser user = mAuth.getCurrentUser();
-//                            updateUI(user);
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                                    Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
-//                        }
-//                    }
-//                });
-//        // [END create_user_with_email]
-//    }
+        binding.alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
